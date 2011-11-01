04a69b571622803a7eac931f02723809546024ef
rainyjune@live.cn <rainyjune@live.cn@ffc42280-1eb7-e787-3974-67a556ae53ea>
2011-11-01 04:21:25 +0000

    Commit

---
+++ emailNotification/config.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'enable'=>1,
+	'Subject'=>'YuanPad notification',
+	'body'=>"%s Left message : %s @ %s",
+	'Host'=>'smtp.163.com',
+	'SMTPAuth'=>1,
+	'Port'=>25,
+	'Username'=>'348106409',
+	'Password'=>'',
+	'to'=>'348106409@163.com',
+);
